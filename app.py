bdf09c12db166bc1834026012c75078e584e2b7c
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-03-08 18:43:36 -0300

    add method to load images

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image
 
 from widget.imageviewer import ImageViewer
 
@@ -13,3 +14,20 @@ class App(tk.Tk):
         self.image_viewer = ImageViewer()
         self.image_viewer.pack()
 
+        # Add test buttons to load images
+        tk.Button(self, text='Cargar original', command=self.on_load_original).pack()
+        tk.Button(self, text='Cargar procesado', command=self.on_load_processed).pack()
+
+    def on_load_original(self):
+        # Open the image from the file
+        self.img_orig = Image.open('Lenaclor.ppm')
+
+        # Set the image in the ImageViewer
+        self.image_viewer.set_original(self.img_orig)
+
+    def on_load_processed(self):
+        # Open the image from the file
+        self.img_proc = Image.open('Lenaclor.ppm')
+
+        # Set the image in the ImageViewer
+        self.image_viewer.set_processed(self.img_proc)
--- widget/imageviewer.py
+++ widget/imageviewer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk
 
 class ImageViewer(tk.Frame):
     def __init__(self):
@@ -13,3 +14,21 @@ class ImageViewer(tk.Frame):
         self.canvas_orig.grid(row=1, column=0)
         self.canvas_proc = tk.Canvas(self, width=600, height=600, bg='grey')
         self.canvas_proc.grid(row=1, column=1)
+
+    def set_original(self, pil_img):
+        # Get canvas dimensions
+        canvasw = self.canvas_orig.winfo_width()
+        canvash = self.canvas_orig.winfo_height()
+
+        # Create the image in the canvas
+        self.photo_orig = ImageTk.PhotoImage(pil_img)
+        self.canvas_orig.create_image(canvasw/2, canvash/2, image=self.photo_orig)
+        
+    def set_processed(self, pil_img):
+        # Get canvas dimensions
+        canvasw = self.canvas_proc.winfo_width()
+        canvash = self.canvas_proc.winfo_height()
+
+        # Create the image in the canvas
+        self.photo_proc = ImageTk.PhotoImage(pil_img)
+        self.canvas_proc.create_image(canvasw/2, canvash/2, image=self.photo_proc)

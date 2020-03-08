041e64cae556f2e7ef96222d7c4ae8c13739085f
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-03-08 17:57:32 -0300

    add button to change class props

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ class App(tk.Tk):
         # Set window title
         self.title('ATI')
 
-        # Test the library with a label
-        label = tk.Label(self, text = 'Hello World!')
+        # Test the library with a label (store as class property to change it later)
+        self.label = tk.Label(self, text = 'Hello World!')
         # Position the label on the window and add padding
-        label.pack(padx=20, pady=20)
+        self.label.pack(padx=20, pady=20)
 
+        # Add a button and set `on_bye` as click handler
+        button = tk.Button(self, text='Bye', command=self.on_bye)
+        # Position the button
+        button.pack()
+
+
+    def on_bye(self):
+        # Update the label
+        self.label.configure(text = 'Bye World!')
     

b50bcad25a4ea51d314a18b3160d20c36fd80875
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-03-18 14:34:59 -0300

    fix remap image function (sum is now working)

--- algo/functions.py
+++ algo/functions.py
@@ -29,35 +29,37 @@ class Functions():
         # load other image
         img_other = self.app_ref.load_image_from_file()
 
-        # Check if both images have the same bands
-        if img_other.getbands() != self.app_ref.img_proc.getbands():
-            # TODO: Show error and do nothing
-            return
-        elif img_other.getbands() == ('R','G','B'):
+        if img_other.getbands() == ('R','G','B') or self.app_ref.img_proc.getbands() == ('R','G', 'B'):
             # TODO: Not implemented for RGB
             return
 
         # TODO: Handle different size images
 
         # Convert images to arrays
-        I1 = np.array(self.app_ref.img_proc.convert('L'))
-        I2 = np.array(img_other.convert('L'))
+        I1 = np.array(self.app_ref.img_proc.convert('L'), dtype=float)
+        I2 = np.array(img_other.convert('L'), dtype=float)
 
+        # Perform function pixel by pixel
         for pix1, pix2 in np.nditer([I1, I2], op_flags=['readwrite']):
             pix1[...] += pix2
 
+
         # Remap image to 0-255
         img = Image.fromarray(self.remap_image_array(I1))
-
-        if img_other.getbands() == ('1',):
-            img = img.convert('1')
 
         self.app_ref.set_processed(img)
 
 
     # This function maps arbitrary
-    # pixel values to 0-255
-    def remap_image_array(self, I):
-        I2 = 255 * (I - np.min(I))/np.ptp(I)
+    # pixel values to 0-max_value
+    def remap_image_array(self, I, max_value=255):
+        # Handle images with range 0
+        if np.ptp(I) == 0:
+            if np.min(I) < 0:
+                return I-np.min(I)
+            elif np.max(I) > max_value:
+                return I-(np.max(I)-max_value)
+            else:
+                return I
 
-        return I2
+        return (I-np.min(I))/np.ptp(I)*max_value
